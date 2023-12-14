957b69cec025909ca6bc8f9e2eb057dbef2241b0
Thiago Martins <pessoal.thiagomartins@gmail.com>
2023-12-14 01:06:20 -0300

    Criação do notificação import

---
+++ app/data_import/import_notificacao.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from entities.notificacao_invalida import NotificacaoInvalida
+from factories.notificacao_factory import criar_notificacao
+
+def importar_notificacao(caminho_arquivo):
+    df = pd.read_excel(caminho_arquivo)
+    notificacoes_validas = []
+    notificacoes_invalidas = []
+    for _, row in df.iterrows():
+        notificacao = criar_notificacao(row)
+        if isinstance(notificacao, NotificacaoInvalida):
+            notificacoes_invalidas.append(notificacao)
+        else:
+            notificacoes_validas.append(notificacao)
+
+    return notificacoes_validas, notificacoes_invalidas
+
