4203e57004217804f7ba556a9d77726183bc2142
Thiago Martins <pessoal.thiagomartins@gmail.com>
2023-12-14 01:05:14 -0300

    Criação do notificação factory

---
+++ app/factories/notificacao_factory.py
@@ -0,0 +1,21 @@
+from entities.notificacao_valida import NotificacaoValida
+from entities.notificacao_invalida import NotificacaoInvalida
+from exceptions.notificacao_exception import ErroNotificacao
+
+def criar_notificacao(dados):
+    try:
+        notificacao = NotificacaoValida(
+            grupo=dados['Grupo'],
+            cota=dados['Cota'],
+            data_envio=dados['DataDeEnvio'],
+            data_retorno=dados['DataDeRetorno'],
+            tipo_retorno=dados['TipoDeRetorno'],
+            escritorio=dados['Escritório'],
+            uf=dados['UF'],
+            cartorio=dados.get('Cartório'),
+            nome=dados.get('Nome'),
+            contrato=dados.get('Contrato')
+        )
+        return notificacao
+    except ErroNotificacao as e:
+        return NotificacaoInvalida(dados, str(e))
