de1ad6ec834d5cd8829a7f39f5d6d214137c328f
Thiago Martins <pessoal.thiagomartins@gmail.com>
2023-12-18 00:22:46 -0300

    ajuste na função de conversão de datas

--- app/utils/conversoes_tipos.py
+++ app/utils/conversoes_tipos.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 
 def converter_data(data_str):
-    # Verificar se 'data_str' já é uma instância de datetime
+    # Verifica se data_str é uma instância de datetime e a converte para string se necessário
     if isinstance(data_str, datetime):
         return data_str
+
     try:
+        # Tenta converter usando o formato dia/mês/ano
         return datetime.strptime(data_str, '%d/%m/%Y')
     except ValueError:
-        raise f"Data inválida: {data_str}"
+        try:
+            # Se falhar, tenta converter usando o formato mês/ano
+            return datetime.strptime(data_str, '%m/%Y')
+        except ValueError:
+            raise ValueError(f"Data inválida: {data_str}")
