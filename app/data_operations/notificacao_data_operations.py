2fc6487b56cb84097e14cc353345ca6fb1cfc700
Thiago Martins <pessoal.thiagomartins@gmail.com>
2023-12-21 00:04:30 -0300

    Corrigindo widget do line chart e ajustando uma operação de dados para o chart exibir "total" ao invés de Notificando

--- app/data_operations/notificacao_data_operations.py
+++ app/data_operations/notificacao_data_operations.py
@@ -3,8 +3,6 @@ from datetime import timedelta
 from utils.conversoes_tipos import converter_data
 import calendar
 
- 
-
 def filtrar_notificacoes(notificacoes, data_inicio=None, data_fim=None, estados=None):
     notificacoes_filtradas = []
     if data_inicio:
@@ -27,6 +25,8 @@ def agrupar_notificacoes_por_tipo_e_data(notificacoes, granularidade='mensal'):
     # Tipos de retorno esperados
     tipos_retorno = ["positiva", "negativa", "notificando"]
     dados_agrupados = {}
+    totais_por_data = {}
+
     for notificacao in notificacoes:
         data_envio = notificacao.get_data_envio()
 
@@ -40,18 +40,29 @@ def agrupar_notificacoes_por_tipo_e_data(notificacoes, granularidade='mensal'):
             dados_agrupados[chave] = 0
         dados_agrupados[chave] += 1
 
-    # Convertendo o dicionário agrupado em um DataFrame para visualização
+        # Acumulando totais por data
+        if data_envio not in totais_por_data:
+            totais_por_data[data_envio] = 0
+        totais_por_data[data_envio] += 1
+
+    # Convertendo o dicionário agrupado em um DataFrame
     df_agrupado = pd.DataFrame(list(dados_agrupados.items()), columns=['DataDeEnvio_Tipo', 'Contagem'])
     df_agrupado[['DataDeEnvio', 'TipoDeRetorno']] = pd.DataFrame(df_agrupado['DataDeEnvio_Tipo'].tolist(), index=df_agrupado.index)
     df_agrupado.drop(columns=['DataDeEnvio_Tipo'], inplace=True)
 
-    # Reestruturando o DataFrame para melhor visualização
+    # Reestruturando o DataFrame
     df_agrupado = df_agrupado.pivot_table(index='DataDeEnvio', columns='TipoDeRetorno', values='Contagem', fill_value=0)
 
-    # Garantindo que todos os tipos de retorno estejam presentes
+    # Garantindo que todos os tipos de retorno e totais estejam presentes
     for tipo in tipos_retorno:
         if tipo not in df_agrupado.columns:
             df_agrupado[tipo] = 0
+    df_agrupado['total'] = df_agrupado.sum(axis=1)
+
+    # Incluindo totais por data de envio
+    for data, total in totais_por_data.items():
+        df_agrupado.at[data, 'total'] = total
+
     return df_agrupado
 
 def agrupar_notificacoes_por_estado(notificacoes):
--- app/widgets/notificacao_line_chart_widget.py
+++ app/widgets/notificacao_line_chart_widget.py
@@ -2,19 +2,29 @@ from PyQt5.QtWidgets import QVBoxLayout, QWidget
 from charts.line_chart import LineChart
 from PyQt5.QtCore import QSize
 
-
 class NotificationLineChartWidget(QWidget):
     def __init__(self, dataframe):
-        super().__init__(parent = None)
-        color_map = {
+        super().__init__(parent=None)
+        self.color_map = {
             'positiva': 'green',
             'negativa': 'red',
-            'notificando': 'blue'
+            'total': 'blue'
         }
         self.layout = QVBoxLayout(self)
-        self.chart = LineChart(dataframe=dataframe, title="Notificações por Tipo",
-                        xlabel="", ylabel="Quantidade", color_map=color_map)
+        self.chart = LineChart(dataframe=self.process_dataframe(dataframe),
+                               title="Notificações por Tipo",
+                               xlabel="", ylabel="Quantidade",
+                               color_map=self.color_map)
         self.layout.addWidget(self.chart)
         self.setMaximumSize(QSize(800, 600))
         self.setMinimumSize(QSize(600, 450))
         self.chart.draw()
+
+    def process_dataframe(self, dataframe):
+        if 'notificando' in dataframe.columns:
+            dataframe = dataframe.drop(columns=['notificando'])
+        return dataframe
+
+    def update_data(self, new_dataframe):
+        processed_dataframe = self.process_dataframe(new_dataframe)
+        self.chart.update_data(processed_dataframe)
